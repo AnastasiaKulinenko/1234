396a0c20a1fbbad30c3d6987c4d4cd386e5360f2
AnastasiaKulinenko <107396344+AnastasiaKulinenko@users.noreply.github.com>
2022-06-13 15:48:56 +0700

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите значение последнего промежутка n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество попыток:");
+int k = Convert.ToInt32(Console.ReadLine());
+
+Random rnd = new Random();
+int N = rnd.Next(1, n);
+while (k != 0)
+{
+    Console.Write("Введите число:");
+    int Namb = Convert.ToInt32(Console.ReadLine());
+    if (Namb == N)
+    {
+        Console.WriteLine("Ты угадал");
+        k = 0;
+    }
+
+    {
+        k--;
+        Console.WriteLine("Ты не угадал");
+        if (Namb <= N)
+            Console.WriteLine("Загаданное число больше");
+        if (Namb >= N)
+            Console.WriteLine("Загаданное число меньше");
+        if (k == 0)
+        {
+            Console.WriteLine("Игра окончена");
+            ;
+        }
+
+
+    }
+}
